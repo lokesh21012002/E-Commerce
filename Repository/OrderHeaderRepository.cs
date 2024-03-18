da5f769a344b8aa863e3edf5fafa2a760a091602
lokesh21012002 <gawandelokesh123@gmail.com>
2024-03-18 19:55:13 +0530

    OrderHeader Repo Implementation

--- Repository/OrderHeaderRepository.cs
+++ Repository/OrderHeaderRepository.cs
@@ -21,17 +21,36 @@ namespace MVC.Repository
 
         public void Update(OrderHeader obj)
         {
-            throw new NotImplementedException();
+            _db.OrderHeaders.Update(obj);
+
         }
 
         public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
         {
-            throw new NotImplementedException();
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+
         }
 
         public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
         {
-            throw new NotImplementedException();
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                orderFromDb.PaymentIntentId = paymentIntentId;
+                orderFromDb.PaymentDate = DateTime.Now;
+            }
         }
     }
 }
